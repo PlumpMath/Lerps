32e59c2ec0abcd433c89d09e412dd5aa9a0a3bca
Ramsey Nasser <ram@nas.sr>
2015-03-26 21:13:05 -0400

    Added speed variable

--- Assets/SphereLerper.js
+++ Assets/SphereLerper.js
@@ -11,11 +11,12 @@ function MoveOver() {
   var t : float = 0;
   var startPoint = Vector3(-5, 0, 0);
   var endPoint = Vector3(5, 0, 0);
+  var speed = 30;
   
   
   while(t < 1) {
     transform.position = startPoint * (1 - Mathf.Pow(t, 2)) + endPoint * Mathf.Pow(t, 2);
-    t += 0.5 * Time.deltaTime;
+    t += 0.5 * Time.deltaTime * speed;
     yield;
   }
 }
